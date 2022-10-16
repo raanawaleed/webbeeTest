4654c5f2d3554f6448ba19c057ff333ce6f11c35
waleed Raza <43804115+raanawaleed@users.noreply.github.com>
2022-10-16 18:08:28 +0500

    Add files via upload

---
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -0,0 +1,103 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+class CreateCinemaSchema extends Migration
+{
+    /** ToDo: Create a migration that creates all tables for the following user stories
+
+    For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
+    To not introduce additional complexity, please consider only one cinema.
+
+    Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
+
+    ## User Stories
+
+     **Movie exploration**
+     * As a user I want to see which films can be watched and at what times
+     * As a user I want to only see the shows which are not booked out
+
+     **Show administration**
+     * As a cinema owner I want to run different films at different times
+     * As a cinema owner I want to run multiple films at the same time in different showrooms
+
+     **Pricing**
+     * As a cinema owner I want to get paid differently per show
+     * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
+
+     **Seating**
+     * As a user I want to book a seat
+     * As a user I want to book a vip seat/couple seat/super vip/whatever
+     * As a user I want to see which seats are still available
+     * As a user I want to know where I'm sitting on my ticket
+     * As a cinema owner I dont want to configure the seating for every show
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('language');
+            $table->string('hour');
+            $table->string('type');
+            $table->timestamps();
+        });
+
+        Schema::create('screening', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('audotorium_id')->references('id')->on('audotorium')->onDelete('cascade');
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->timestamps();
+        });
+
+        Schema::create('administration', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('screening_id')->references('id')->on('screening')->onDelete('cascade');
+            $table->string('name');
+            $table->string('is_active');
+            $table->timestamps();
+        });
+
+        Schema::create('pricing', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('seat_id')->references('id')->on('seating')->onDelete('cascade');
+            $table->string('seat_type_price');
+            $table->integer('premium_percentage')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::create('audotorium', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('seat_no');
+            $table->timestamps();
+        });
+
+        Schema::create('seating', function (Blueprint $table) {
+            $table->id();
+            $table->string('row');
+            $table->string('number');
+            $table->foreign('audotorium_id')->references('id')->on('audotorium')->onDelete('cascade');
+            $table->timestamps();
+        });
+        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+        Schema::dropIfExists('administration');
+        Schema::dropIfExists('pricing');
+        Schema::dropIfExists('seating');
+    }
+}

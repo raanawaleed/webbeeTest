f65f59cdc7a6d07e755b951aef2e00e1a394f74e
waleed Raza <43804115+raanawaleed@users.noreply.github.com>
2022-10-16 18:29:12 +0500

    Add files via upload

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,17 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+    public function childs() {
+        return $this->hasMany(MenuItem::class,'parent_id','id') ;
+    }
+
+    public function getAllItems(){
+        if($this->child->count() > 0)
+        {
+            $this->child->getAll();
+        }
+
+        return $this;
+    }
+
 }
